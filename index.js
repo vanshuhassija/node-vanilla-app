87a26196fcc58669df8dd74263db2f0da4b54f07
vanshuhassija <vanshuhassija@gmail.com>
2022-03-07 22:17:32 +0530

    Expresss Dominos App

--- index.js
+++ index.js
@@ -5,39 +5,87 @@
 //2. Send the headers
 //3. Send the body of resp
 
-const { response } = require("express");
-const http=require("http");
+const express= require("express");
+const app= express();
 
-const server=http.createServer((req,res)=>{
-   if(req.url.startsWith("/welcome")){
-    //    res.writeHead(statusCode,headersObject)
-       res.writeHead(200,{
-              "Content-Type":"text/plain"
-       })
-       //writeHead is a mehod of response object which is used to
-       // send the status code and the headers
-       res.end("Welcome to Dominos");
-   }
-
-   if(req.url.startsWith("/contact")){
-         res.writeHead(200,{
-              "Content-Type":"application/json"
-         })
-         res.end(JSON.stringify({
-              name:"Dominos",
-              address:"Bangalore",
-              contact:"9876543210"
-         }));
-   }
+//Define Routes in express
+// app.method(path,callback)
 
 
-   res.writeHead(404);
-   res.end("Page Not Found");
-
-    // res.end("Welcome to Dominos");
-    //res.end ----> Terminates the response and sends the response to the client with a message.
+app.get("/welcome",(req,res)=>{
+    // // res.writeHead(statusCode,headersObject)
+    //    res.writeHead(200,{
+    //           "Content-Type":"text/plain"
+    //    })
+    //    //writeHead is a mehod of response object which is used to
+    //    // send the status code and the headers
+    //    res.end("Welcome to Dominos");
+    res.send("Welcome to Dominos");
+    // res.write() + res.end()
 })
 
-server.listen(8080,()=>{
-    console.log("Server Started on 8080");
+app.get("/contact",(req,res)=>{
+    // res.writeHead(200,{
+    //     "Content-Type":"application/json"
+    // })
+    // res.end(JSON.stringify({
+    //     name:"Dominos",
+    //     address:"Bangalore",
+    //     contact:"9876543210"
+    // }))
+    res.send({
+        name:"Dominos",
+        address:"Bangalore",
+        contact:"9876543210"
+    })
 })
+
+app.get("*",(req,res)=>{
+    res.writeHead(400,{
+        "Content-Type":"text/html"
+    })
+    res.end("<h1>Not Found</h1>")
+})
+
+app.listen(8080,()=>{
+    console.log("Started Server on 8080")
+})
+
+
+
+
+// const http=require("http");
+
+// const server=http.createServer((req,res)=>{
+//    if(req.url.startsWith("/welcome")){
+//     //    res.writeHead(statusCode,headersObject)
+//        res.writeHead(200,{
+//               "Content-Type":"text/plain"
+//        })
+//        //writeHead is a mehod of response object which is used to
+//        // send the status code and the headers
+//        res.end("Welcome to Dominos");
+//    }
+
+//    if(req.url.startsWith("/contact")){
+//          res.writeHead(200,{
+//               "Content-Type":"application/json"
+//          })
+//          res.end(JSON.stringify({
+//               name:"Dominos",
+//               address:"Bangalore",
+//               contact:"9876543210"
+//          }));
+//    }
+
+
+//    res.writeHead(404);
+//    res.end("Page Not Found");
+
+//     // res.end("Welcome to Dominos");
+//     //res.end ----> Terminates the response and sends the response to the client with a message.
+// })
+
+// server.listen(8080,()=>{
+//     console.log("Server Started on 8080");
+// })
